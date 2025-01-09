5e38081e399ec8ca35991d0655a41dd762ccc9e0
JasonBelmiro <jason.belmiro@gmail.com>
2025-01-09 11:04:02 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Dashboard from './Dashboard';
 import Chatbot from './Chatbot';
 import Contact from './Contact';
 import MessageLog from './MessageLog';
-import AIReporting from './AI-Reporting';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Route path="chatbot" element={<Chatbot />} />
       <Route path="contact" element={<Contact />} />
       <Route path="message-log" element={<MessageLog />} />
-      <Route path="ai-reporting" element={<AIReporting />} />
     </Routes>
   );
 }
